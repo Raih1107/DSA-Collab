edf8395cb00750b378ac292622c44203ad5fde1c
Nitin Chauhan <nitinchauhan0808@gmail.com>
2025-06-13 14:07:09 +0530

    two_sum_sol

---
+++ solutions/nitin/two_sum.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define endl '\n'
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> input(n);
+    for (int &i : input)
+    {
+        cin >> i;
+    }
+    return 0;
+}
