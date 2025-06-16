25d9b9a6217a6d3023f5197142ec8b9179d7d2ae
prerak1310 <preraksangwan@gmail.com>
2025-06-16 10:28:17 +0530

    two-sum solution

--- solutions/prerak/two_sum.cpp
+++ solutions/prerak/two_sum.cpp
@@ -2,10 +2,28 @@
 using namespace std;
 
 
+vector<int> twosum(vector<int>& nums, int target){
+	unordered_map<int,int> mp;
+	for(int i = 0; i < nums.size(); i++){
+		int num = nums[i];
+		if(mp.find(target - num) != mp.end()){
+			return {i, mp[num]};
+		}
+		mp[num] = i;
+	}
+	return {};
+
+}
+
+
 
 int main(){
-	int nums = {2,7,11,15};
+	vector<int> nums = {2,7,11,15};
 	int target = 9;
+	vector<int> ans = twosum(nums, target);
+	for(int x:ans){
+		cout << x << endl;
+	}
 	return 0;
 
 }
