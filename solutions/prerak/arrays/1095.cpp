78d64f9061faa41af691d4be2bc3faa0d5d9d576
prerak1310 <preraksangwan@gmail.com>
2025-07-16 23:16:11 +0530

    prerak Leetcode 1095.

--- solutions/prerak/arrays/1095.cpp
+++ solutions/prerak/arrays/1095.cpp
@@ -1,10 +1,75 @@
-// TC --> O(N) and SC --> O(N)
-
+// TC --> O(logN) and SC --> O(1)
 #include <bits/stdc++.h>
 using namespace std;
 
+class MountainArray {
+private:
+  vector<int> arr;
 
+public:
+  MountainArray(const vector<int> &v) : arr(v) {}
 
-int main(){
+  int get(int index) {
+    if (index >= 0 && index < arr.size())
+      return arr[index];
+    throw out_of_range("Index out of bounds");
+  }
 
+  int length() { return arr.size(); }
+};
+
+int findPeak(MountainArray &mountainArr) {
+  int left = 0;
+  int right = mountainArr.length() - 1;
+
+  while (left < right) {
+    int mid = left + (right - left) / 2;
+    if (mountainArr.get(mid) < mountainArr.get(mid + 1))
+      left = mid + 1;
+    else
+      right = mid;
+  }
+
+  return left;
+}
+
+int findInMountainArray(int target, MountainArray &mountainArr) {
+  int peakIndex = findPeak(mountainArr);
+
+  // Binary search in ascending part
+  int left = 0, right = peakIndex;
+  while (left <= right) {
+    int mid = left + (right - left) / 2;
+    int val = mountainArr.get(mid);
+    if (val == target)
+      return mid;
+    if (val < target)
+      left = mid + 1;
+    else
+      right = mid - 1;
+  }
+
+  // Binary search in descending part
+  left = peakIndex + 1, right = mountainArr.length() - 1;
+  while (left <= right) {
+    int mid = left + (right - left) / 2;
+    int val = mountainArr.get(mid);
+    if (val == target)
+      return mid;
+    if (val > target)
+      left = mid + 1;
+    else
+      right = mid - 1;
+  }
+
+  return -1;
+}
+
+int main() {
+  vector<int> rawArr = {1, 2, 3, 4, 5, 3, 1};
+  int target = 3;
+  MountainArray mountainArr(rawArr);
+  cout << findInMountainArray(target, mountainArr) << endl;
+
+  return 0;
 }
--- solutions/prerak/hashmap/1.cpp
+++ solutions/prerak/hashmap/1.cpp
@@ -3,29 +3,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
-vector<int> twosum(vector<int>& nums, int target){
-	unordered_map<int,int> mp;
-	for(int i = 0; i < nums.size(); i++){
-		int num = nums[i];
-		if(mp.find(target - num) != mp.end()){
-			return {i, mp[num]};
-		}
-		mp[num] = i;
-	}
-	return {};
-
+vector<int> twosum(vector<int> &nums, int target) {
+  unordered_map<int, int> mp;
+  for (int i = 0; i < nums.size(); i++) {
+    int num = nums[i];
+    if (mp.find(target - num) != mp.end()) {
+      return {i, mp[num]};
+    }
+    mp[num] = i;
+  }
+  return {};
 }
 
-
-
-int main(){
-	vector<int> nums = {2,7,11,15};
-	int target = 9;
-	vector<int> ans = twosum(nums, target);
-	for(int x:ans){
-		cout << x << endl;
-	}
-	return 0;
-
+int main() {
+  vector<int> nums = {2, 7, 11, 15};
+  int target = 9;
+  vector<int> ans = twosum(nums, target);
+  for (int x : ans) {
+    cout << x << endl;
+  }
+  return 0;
 }
